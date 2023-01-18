9eff6219eb0fc2cf5b05452b9cf1d541aab91b6a
kleydsonpessanhapereira@gmail.com <kleydsonpessanhapereira@gmail.com>
2023-01-18 20:02:38 -0300

    criei a parte do menu footer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+
 const controls = document.querySelectorAll(".control");
 let currentItem = 0;
 const items = document.querySelectorAll(".item");
 const maxItems = items.length;
+
 
 controls.forEach((control) => {
   control.addEventListener("click", (e) => {
